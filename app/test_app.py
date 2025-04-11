03a86a7336bd03063f8e82b6f17a9c43ebef2076
Guillermo Rodrigues <grodrigues217@gmail.com>
2025-04-11 11:38:48 +0200

    ci: correccion sintasis test_app.py

--- app/test_app.py
+++ app/test_app.py
@@ -2,6 +2,7 @@ import pytest
 from app import app, db
 from unittest.mock import MagicMock, patch
 
+
 @pytest.fixture
 def client():
     """Creates a Flask test client"""
@@ -13,11 +14,13 @@ def client():
     with app.test_client() as client:
         yield client
 
+
 def test_index_success(client):
     """Successful test: Verifies that the route '/' responds with status code 200"""
     response = client.get("/")
     assert response.status_code == 200
     assert "La página ha sido cargada" in response.get_data(as_text=True)
+
 
 def test_index_exception(client):
     with patch("app.es.index", side_effect=Exception("Fallo en Index")):
@@ -28,15 +31,18 @@ def test_index_exception(client):
         assert "error" in data
         assert data["error"] == "Fallo en Index"
 
+
 def test_not_found(client):
     """Failed test: Tries to access a non-existent route"""
     response = client.get("/notfound")
     assert response.status_code == 404
 
+
 def test_health_success(client):
     """Successful test: Verifies that the route '/health' responds with status code 200"""
     response = client.get("/health")
     assert response.status_code == 200
+
 
 def test_health_exception(client):
     with patch("app.es.index", side_effect=Exception("Fallo en Health")):
@@ -47,10 +53,12 @@ def test_health_exception(client):
         assert "error" in data
         assert data["error"] == "Fallo en Health"
 
+
 def test_logs_success(client):
     """Successful test: Verifies that the route '/logs' responds with status code 200"""
     response = client.get("/logs")
     assert response.status_code == 200
+
 
 def test_logs_exception(client):
     with patch("app.es.search", side_effect=Exception("Fallo en Elasticsearch")):
@@ -60,6 +68,7 @@ def test_logs_exception(client):
         data = response.get_json()
         assert "error" in data
         assert data["error"] == "Fallo en Elasticsearch"
+
 
 # Test that always fails intentionally to see what happens in pytest. Uncomment to see the result.
 # def test_forced():
